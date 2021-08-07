eed9f142924085f2d20e51b673237cf2af5c3590
Prajwal <61734018+prajwalr308@users.noreply.github.com>
2021-08-07 22:17:00 +0530

    mental models-values

---
+++ values.js
@@ -0,0 +1,40 @@
+//Primitive values
+/*
+They are a permanent part of our JavaScript universe.
+I can point to them, but I can’t create, destroy, or change them.
+*/
+//To see primitive values in practice, open your browser’s console and log them:
+
+console.log(2);
+console.log("hello");
+console.log(undefined);
+
+//Objects and Functions
+/*
+
+Objects and functions are also values but, unlike primitive values, I can manipulate them from my code. 
+If primitive values are like distant stars, then objects and functions are more like asteroids floating around my planet.
+They’re not part of my code, but they’re close enough to manipulate
+*/
+console.log({});
+console.log([]);
+console.log(x => x * 2);
+
+//checking types
+console.log(typeof([])); 
+console.log(typeof(new Date())); 
+console.log(typeof(/(hello|goodbye)/))
+
+
+
+console.log(typeof({})); // "object"
+console.log(typeof([])); // "object"
+console.log(typeof (x => x * 2));
+
+//Expressions
+
+console.log(2 + 2); 
+//2+2
+console.log(typeof(2 + 2))
+//you can perform type of for expressions also
+
